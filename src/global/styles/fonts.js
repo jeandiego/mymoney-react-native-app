fd052b5e42a9ce5c212e1f73530d42b3c383996a
Jean Diego <jeandiego.ds@gmail.com>
2021-09-30 15:52:42 -0300

    💄 Updated global theming style

--- src/global/styles/fonts.js
+++ src/global/styles/fonts.js
@@ -1,6 +1,6 @@
 export const fonts = {
-  regular: 'Poppins-Light',
-  light: 'Poppins-Regular',
+  regular: 'Poppins-Regular',
+  light: 'Poppins-Light',
   medium: 'Poppins-Medium',
   bold: 'Poppins-Bold',
 };
--- src/global/styles/theme.js
+++ src/global/styles/theme.js
@@ -1,14 +1,8 @@
 export const common = {
-  positive: '#12A454',
-  positive_light: 'rgba(18, 164, 84, 0.5)',
-
-  negative: '#E83F5B',
-  negative_light: 'rgba(232, 63, 91, 0.5)',
-
   white: '#FFFFFF',
   black: '#000000',
 
-  opacity: 'rgba(0,0,0,0.8)',
+  opacity: 'rgba(0,0,0,0.1)',
 };
 
 export const lightTheme = {
@@ -19,10 +13,25 @@ export const lightTheme = {
   secondary: '#FF872C',
   secondary_light: 'rgba(255, 135, 44, 0.3)',
 
+  positive: '#12A454',
+  positive_light: 'rgba(18, 164, 84, 0.1)',
+
+  negative: '#E83F5B',
+  negative_light: 'rgba(232, 63, 91, 0.1)',
+
+  regular: '#30B8D6',
+  regular_light: '#BAECF3',
+
   shape: '#FFFFFF',
   title: '#363F5F',
   text: '#969CB2',
   background: '#F0F2F5',
+
+  gradient01: '#002780',
+  gradient02: '#2A80D1',
+
+  gradiente03: '#32DF94',
+  gradient04: '#35BCD9',
 
   shadow: 'rgba(0,0,0,0.4)',
 };
@@ -35,10 +44,25 @@ export const darkTheme = {
   secondary: '#FF872C',
   secondary_light: 'rgba(255, 135, 44, 0.3)',
 
-  shape: '#FFFFFF',
+  positive: '#207E46',
+  positive_light: '#C7F1D1',
+
+  negative: '#CA4159',
+  negative_light: '#F3BABA',
+
+  regular: '#30B8D6',
+  regular_light: '#BAECF3',
+
+  shape: '#1F1E25',
   title: '#363F5F',
   text: '#969CB2',
-  background: '#F0F2F5',
+  background: '#121015',
+
+  gradient01: '#002780',
+  gradient02: '#2A80D1',
+
+  gradiente03: '#32DF94',
+  gradient04: '#35BCD9',
 
   shadow: 'rgba(0,0,0,0.4)',
 };

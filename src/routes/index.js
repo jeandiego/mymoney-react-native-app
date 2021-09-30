57f5f149ce3d401f732fbbcec6b0f724394dcc91
Jean Diego <jeandiego.ds@gmail.com>
2021-09-30 15:51:15 -0300

    🚚 Changed routes path

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,19 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { navigationRef } from './RootNavigation';
-import Home from '~/screens/Home';
 
-const { Navigator, Screen } = createStackNavigator();
+import { AuthRoutes } from './auth.routes';
+import { AppRoutes } from './app.routes';
+
+import { useAuth } from '../hooks/auth';
 
 export function Routes() {
+  const { user } = useAuth();
+
   return (
     <NavigationContainer ref={navigationRef}>
-      <Navigator screenOptions={{ headerShown: false }}>
-        <Screen name="Home" component={Home} />
-      </Navigator>
+      {user._id ? <AppRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }

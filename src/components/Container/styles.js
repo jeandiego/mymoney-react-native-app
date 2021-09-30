24bb11241809e78bf3b30bb5f0684421c24385c4
Jean Diego <jeandiego.ds@gmail.com>
2021-09-30 15:52:26 -0300

    💄 Improvements on global container

--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -4,7 +4,10 @@ export const Container = styled.View`
   flex: 1;
   background-color: ${(p) =>
     p.theme.colors[p.background] || p.theme.colors.background};
-  padding: 0 16px;
-  padding-bottom: ${(p) => p.safeArea.bottom + 24 || 0}px;
-  padding-top: ${(p) => p.safeArea.top + 24 || 0}px;
+  padding-left: ${(p) => p.pLeft || p.pHorizontal || 0}px;
+  padding-right: ${(p) => p.pRight || p.pHorizontal || 0}px;
+  padding-bottom: ${(p) => p.pBottom || p.pVertical || 0}px;
+  padding-top: ${(p) => p.pTop || p.pVertical || 0}px;
+  padding-bottom: ${(p) => p.safeArea.bottom || 0}px;
+  padding-top: ${(p) => p.safeArea.top || 0}px;
 `;

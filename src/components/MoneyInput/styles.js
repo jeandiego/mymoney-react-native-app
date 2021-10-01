4a88e3836c00e101731b09fcb1a99f9a8c899740
Jean Diego <jeandiego.ds@gmail.com>
2021-09-30 21:47:42 -0300

    ✨ Improvements on global input component

--- src/components/MoneyInput/styles.js
+++ src/components/MoneyInput/styles.js
@@ -1,17 +1,21 @@
 import { Platform } from 'react-native';
-import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
+import { fonts } from '~/global/styles/fonts';
 
-export const Container = styled.TextInput`
+export const Container = styled.TextInput.attrs((p) => ({
+  placeholderTextColor: p.theme.colors[p.placeColor] || p.theme.colors.opacity,
+}))`
   width: 100%;
   background-color: ${(p) => p.theme.colors[p.background] || 'transparent'};
-  color: ${({ theme }) => theme.colors.title};
-  font-size: ${RFValue(14)}px;
-  padding: ${Platform.OS === 'ios' ? 15 : 10}px;
+  color: ${(p) => p.theme.colors[p.color] || p.theme.colors.title};
+  padding: ${Platform.OS === 'ios' ? 12 : 10}px;
   padding-left: 0;
+  text-align: ${(p) => (p.alignText ? p.alignText : 'left')};
   border-bottom-color: ${(p) =>
     p.theme.colors[p.background] || p.isFocused
       ? p.theme.colors.primary
-      : p.theme.colors.text};
-  border-bottom-width: 3px;
+      : p.theme.colors.opacity};
+  border-bottom-width: ${(p) => p.borderBottom || 3}px;
+  font-family: ${(p) => fonts[p.weight] || 'Poppins-Regular'};
+  font-size: ${(p) => p.size || 16}px;
 `;

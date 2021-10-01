a73380fa9838ff811bc438f2b7d5551975c43865
Jean Diego <jeandiego.ds@gmail.com>
2021-09-30 21:51:53 -0300

    ✨ Created releases screen

---
+++ src/screens/Releases/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReleasesView from './view';
+
+const Releases = () => {
+  return <ReleasesView />;
+};
+
+export default Releases;
---
+++ src/screens/Releases/view.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalContainer from '~/components/Container';
+import MoneyText from '~/components/MoneyText';
+
+const ReleasesView = () => {
+  return (
+    <GlobalContainer>
+      <MoneyText>Tela Lançamentos</MoneyText>
+    </GlobalContainer>
+  );
+};
+
+export default ReleasesView;
